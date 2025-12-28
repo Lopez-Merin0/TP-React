2880bb13592fd80e85f5fdfcc58f5140cdc9eaf5
Fernanda 3 <maria.merino@uabc.edu.mx>
2025-12-27 21:56:13 -0800

    se creo componente TodoList para renderizar la lista de tareas

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,24 @@
+import Todo from './Todo.jsx';
+
+const TodoList = ({ todos, deleteTodo, toggleTodo, editTodo }) => {
+    return (
+        <div className="todo-list">
+            {todos.lenght === 0 ? (
+                <p className="empty-msg">No hay tareas pendientes</p>
+            ) : (
+                todos.map((todo) => (
+                    <Todo
+                        key={todo.id}
+                        todo={todo}
+                        deleteTodo={deleteTodo}
+                        toggleTodo={toggleTodo}
+                        editTodo={editTodo}
+                    />
+                ))
+            )}
+
+        </div>
+    );
+};
+
+export default TodoList;
